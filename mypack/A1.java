b53a60e51d6a5db57ce5aa2e4347cb31d42d4cae
Rehan-r7 <111696261+Rehan-r7@users.noreply.github.com>
2023-02-25 16:13:31 +0530

    Add files via upload

---
+++ mypack/A1.java
@@ -0,0 +1,9 @@
+//create three packages p1,p2,p3 create another class and import classes of all packages
+package p1;
+
+public class A1 {
+    public void show1()
+    {
+        System.out.println("Hello I am class A1 of p1");
+    }
+}
