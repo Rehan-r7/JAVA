b07532c448af5eb490edc39b7e6c060cd3695374
Rehan-r7 <111696261+Rehan-r7@users.noreply.github.com>
2023-02-28 20:10:32 +0530

    Update Ex_Scanner2.java

--- Basic_Programs/Ex_Scanner2.java
+++ Basic_Programs/Ex_Scanner2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Ex_Scanner2{
     int x;
     public static void main(String[] args) {
-        CrObj ob = new CrObj();
+       Ex_Scanner2 ob = new Ex_Scanner2();
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Please enter the value : ");
             ob.x = sc.nextInt();
