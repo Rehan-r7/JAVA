efcd0710b31f384e6263323b858fbd6df528fa44
Rehan-r7 <111696261+Rehan-r7@users.noreply.github.com>
2023-02-28 20:11:44 +0530

    Update Volume.java

--- Basic_Programs/Volume.java
+++ Basic_Programs/Volume.java
@@ -1,15 +1,11 @@
 //wap in java to calculate volume of box display result
-public class Volume{
-  
-        
-       int l=2;
+public class Volume
+{       int l=2;
         int b=3;
         int h=4;
-   
-   
-   
+ 
     public static void main(String args[]) {
-       prg1 obj= new prg1();
+       Volume obj= new Volume();
        int vol;
         vol=obj.l*obj.b*obj.h;
         System.out.println("Volume="+vol);
