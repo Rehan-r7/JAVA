574f910dafdf8577faeb5bf9df4d97f246d48261
Rehan-r7 <111696261+Rehan-r7@users.noreply.github.com>
2023-02-28 19:54:32 +0530

    Create Create_obj.java

---
+++ Basic_Programs/Create_obj.java
@@ -0,0 +1,12 @@
+public class object {
+   
+   int x=10;
+   int y=20;
+   
+    public static void main(String args[]) {
+   
+        object obj=new object();
+        System.out.println(obj.x +"\t"+obj.y);
+     
+    }
+}
