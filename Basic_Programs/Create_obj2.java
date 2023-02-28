6879fbcafec283af5044ad2c9e76389f254ad014
Rehan-r7 <111696261+Rehan-r7@users.noreply.github.com>
2023-02-28 19:57:52 +0530

    Create Create_obj2.java

---
+++ Basic_Programs/Create_obj2.java
@@ -0,0 +1,14 @@
+public class Create_obj2{
+   
+   int x;
+   int y;
+   
+    public static void main(String args[]) {
+   
+        obj_sum obj=new obj_sum();
+        obj.x=3;
+        obj.y=5;
+        System.out.println(obj.x+obj.y);
+     
+    }
+} 
