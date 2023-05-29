c864fbc7c49a00e5547b11c210557660192bdd19
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-05-29 22:52:09 +0530

    Update Search2D.java

--- Array/Search2D.java
+++ Array/Search2D.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 public class Search2D {
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter the Row and Column size : ");
             int row = sc.nextInt(); 
             int col = sc.nextInt();
             int arr[][] = new int[row][col];
             
             //input
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
+            System.out.print("Enter the Elements in the Array :-\n");
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
                     arr[i][j] = sc.nextInt();
                 }
             }
             System.out.print("Enter an element to search : ");
             int x = sc.nextInt();
             //search given element
-            for (int i = 0; i < arr.length; i++) {
-                for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < row; i++) {
+                for (int j = 0; j < col; j++) {
                     if(arr[i][j] == x)
                     System.out.println("Element found at index arr["+i+"]["+j+"]");
                 }
