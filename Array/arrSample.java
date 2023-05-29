bbc2b26ab65ee5939e284b17330ba8c6850335d4
Rehan khan <111696261+Rehan-r7@users.noreply.github.com>
2023-05-29 22:34:45 +0530

    Create arrSample.java

---
+++ Array/arrSample.java
@@ -0,0 +1,16 @@
+public class arrSample{
+public static void main(String[] args) {
+        int arr[] = new int[5]; 
+        arr[0] = 5;
+        arr[1] = 4;
+        arr[2] = 3;
+        arr[3] = 2;
+        arr[4] = 1;
+        System.out.println("\nArray  : ");
+        
+        for(int i = 0;i<5;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
